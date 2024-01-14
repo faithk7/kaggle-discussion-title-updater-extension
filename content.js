f35c3563e454454f66844f5a3250d5b9c518fa0b
faithk7u <faithk7u@outlook.com>
2024-01-14 09:41:01 -0500

    used mutationObserver to make the extension work

--- content.js
+++ content.js
@@ -1,15 +1,19 @@
 // content.js
 
-console.log("Test Extension loaded.");
+const observer = new MutationObserver(mutations => {
+    mutations.forEach(mutation => {
+        if (mutation.addedNodes.length) {
+            const discussionTitle = document.querySelector('h3[emphasis="high"]'); // this is the css/html that can help to identify the discussion title
+            if (discussionTitle) {
+                document.title = discussionTitle.textContent.trim();
+                console.log("Discussion title found and tab title updated.");
+                observer.disconnect();
+            }
+        }
+    });
+});
 
-function logDOMLoaded() {
-    console.log("DOM fully loaded and parsed.");
-}
-
-if (document.readyState === "complete" || document.readyState === "interactive") {
-    console.log("inside the if branch");
-    logDOMLoaded();
-} else {
-    console.log("else branch");
-    document.addEventListener('DOMContentLoaded', logDOMLoaded);
-}
+// Configuration of the observer:
+const config = { childList: true, subtree: true };
+// Start observing the body for added nodes
+observer.observe(document.body, config);
