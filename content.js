04bac093c2d8ca4583a3c6fa3e2983556d410a70
faithk7u <faithk7u@outlook.com>
2024-01-21 14:56:03 -0500

    updated the query selector needed to get the title as well as rendering the discussion emoji instead of the tex

--- content.js
+++ content.js
@@ -3,9 +3,9 @@
 const observer = new MutationObserver(mutations => {
     mutations.forEach(mutation => {
         if (mutation.addedNodes.length) {
-            const discussionTitle =  document.querySelector('h3[emphasis="high"]'); // this is the css/html that can help to identify the discussion title
+            const discussionTitle =  document.querySelector('div[data-testid="discussions-topic-header"] h3'); // this is the css/html that can help to identify the discussion title, seems dynamically changing
             if (discussionTitle) {
-                document.title = "Discussion | " + discussionTitle.textContent.trim();
+                document.title = "💬 | " + discussionTitle.textContent.trim();
                 console.log("Discussion title found and tab title updated.");
                 observer.disconnect();
             }
